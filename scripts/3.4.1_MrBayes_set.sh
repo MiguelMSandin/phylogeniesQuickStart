808d9e23859f9f914d36d9e02ddc5446484bd090
MiguelMSandin <miguelmendezsandin@gmail.com>
2023-03-10 10:49:31 +0100

    Update 3.4.1_MrBayes_set.sh

--- scripts/3.4.1_MrBayes_set.sh
+++ scripts/3.4.1_MrBayes_set.sh
@@ -1,8 +1,8 @@
-
-set autoclose=yes nowarnings=yes
-execute all_filtered_align-auto_trim05.nexus
-lset nst=6 rates=gamma
-mcmc ngen=10000000 Nruns=3 savebrlens=yes file=all_filtered_align-auto_trim05_mrBayes
-sump
-sumt
-quit
+begin mrbayes;
+	set autoclose=yes nowarn=yes;
+	execute FILE.nexus;
+	lset nst=6 rates=gamma;
+	mcmc ngen=10000000 Nruns=4 savebrlens=yes file=OUTPUT;
+	sump;
+	sumt;
+end;
